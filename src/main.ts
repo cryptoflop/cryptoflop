483f4d9364db4b7ff5b26338d232fc09a5abf2da
Cryptoflop <max.david1007@gmail.com>
2024-04-06 17:31:33 +0200

    add base64 font

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
+import './font.css'
 import './main.css'
 import './crt.css'
 import App from './App.svelte'
 
 import cursor from './assets/images/cursor.png';
-import pointCursor from './assets/images/cursor-point.png';
 
 import favicon from './assets/favicon.png';
 
 document.documentElement.style.setProperty('--cursor-default', `url(${cursor}), auto`);
-document.documentElement.style.setProperty('--cursor-pointer', `url(${pointCursor}), auto`);
 
 document.getElementById("favicon")!.setAttribute("href", favicon)
 
 const app = new App({
-	target: document.getElementById('app')!,
+	target: document.getElementById('app')!
 })
 
 export default app
